7cdc8af62cd1eb10eb5b7dc18a7751c3bc07268d
martynakazmierczyk000-oss <martyna.kazmierczyk000@gmail.com>
2025-12-06 12:28:00 +0100

    dodanie informacji szczegółowych

--- import requests.py
+++ import requests.py
@@ -2,7 +2,7 @@ import requests
 #Dodawanie naszego klucza API
 
 api_key='912b2e482a824a7688b81205250612'
-city='Lublin' 
+city='Warszawa' 
 
 #Utworzenie zapytania do API OpenWeatherMap
 url=f'https://api.weatherapi.com/v1/current.json?key=912b2e482a824a7688b81205250612&q=Warszawa&aqi=yes'
@@ -12,4 +12,13 @@ response=requests.get(url)
 response=response.json()
 
 #Wyświetlamy pobrane dane
-print(response)
+#print(response)
+
+#Wyświetlenie szczegółowych elementów
+print(f'Temperatura dla miasta {city} wynosi {response["current"]["temp_c"]}°C')
+print(f'Wilgotność powietrza dla masta {city} wynosi {response["current"]["humidity"]}%')
+print(f'Ciśnienie dla miasta {city} wynosi {response["current"]["pressure_mb"]} hPa')
+
+#Informacje ogolne o pogodzie
+weather_condition=response['current']['condition']['text']
+print(f'Warunki pogodowe dla miasta {city} to {weather_condition}')
